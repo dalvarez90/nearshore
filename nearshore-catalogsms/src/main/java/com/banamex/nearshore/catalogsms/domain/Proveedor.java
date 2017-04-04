bec5fc43ba8247ac67b006e981d9ae80b719b2af
= <hconde@anzen.com.mx>
2017-04-03 19:39:16 -0500

    Limpieza y orden del código

--- nearshore-catalogsms/src/main/java/com/banamex/nearshore/catalogsms/domain/Proveedor.java
+++ nearshore-catalogsms/src/main/java/com/banamex/nearshore/catalogsms/domain/Proveedor.java
@@ -4,18 +4,21 @@ public class Proveedor {
 
 	private Integer id;
 	private String descripcion;
-	
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getDescripcion() {
 		return descripcion;
 	}
+
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
-	
+
 }
